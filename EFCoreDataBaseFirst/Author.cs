c8ca3b6d591b5da363e9aaf851c990e9d7f760fd
YuriiTrofimenko <tyaa@ukr.net>
2021-06-04 20:45:53 +0300

    db first generate n try

---
+++ EFCoreDataBaseFirst/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreDataBaseFirst
+{
+    public partial class Author
+    {
+        public Author()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
--- EFCoreDataBaseFirst/Program.cs
+++ EFCoreDataBaseFirst/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Channels;
 
 namespace EFCoreDataBaseFirst
 {
@@ -7,6 +9,13 @@ namespace EFCoreDataBaseFirst
         static void Main(string[] args)
         {
             Console.WriteLine("Hello EF Core!");
+            LibraryContext context = new LibraryContext();
+            context.Books.Select(b => $"{b.Title} ({b.Author.Name})")
+                .ToList().ForEach(Console.WriteLine);
+            /* Author a = new Author() {Name = "June Four"};
+            context.Authors.Add(a);
+            context.Books.Add(new Book() {Title = "B4", Author = a});
+            context.SaveChanges(); */
         }
     }
 }
