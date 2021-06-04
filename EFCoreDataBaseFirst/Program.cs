91da505e717e3b036402ea71f562f973c333739a
YuriiTrofimenko <tyaa@ukr.net>
2021-06-04 20:19:26 +0300

    init

---
+++ EFCoreDataBaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreDataBaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello EF Core!");
+        }
+    }
+}
