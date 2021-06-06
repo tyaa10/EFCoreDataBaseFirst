8afadb3e334a965fa3e58dd81a3abe46d1928b86
Sesa_oq56 <Sesa_oq56@student.itstep.org>
2021-06-06 21:06:45 +0300

    Remove_Group

--- EFCoreDataBaseFirst/LibraryContext.cs
+++ EFCoreDataBaseFirst/LibraryContext.cs
@@ -29,12 +29,12 @@ namespace EFCoreDataBaseFirst
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Data Source=192.168.0.106,1433;Initial Catalog=Library;Integrated Security=False;user id=sa;password=Passw0rd%");
-                var config =
-                    new ConfigurationBuilder()
-                        .AddEnvironmentVariables()
-                        .Build();
-                optionsBuilder.UseSqlServer(config["CONNECTION_STRING"]);
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True");
+                //var config =
+                //    new ConfigurationBuilder()
+                //        .AddEnvironmentVariables()
+                //        .Build();
+                //optionsBuilder.UseSqlServer(config["CONNECTION_STRING"]);
             }
         }
 
