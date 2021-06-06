2554a7bb80af94155eb41fd4fddf0a6ffc0feaa8
YuriiTrofimenko <tyaa@ukr.net>
2021-06-06 18:31:37 +0300

    linq to sql (groups, students) demo

--- EFCoreDataBaseFirst/Program.cs
+++ EFCoreDataBaseFirst/Program.cs
@@ -60,22 +60,39 @@ namespace EFCoreDataBaseFirst
             context.Entry(result).Reference(b => b.Author).Load();
             Console.WriteLine(result.Author != null ? result.Author.Name : "No Author Info");
             // Console.WriteLine($"{result.Name} ({context.Books.Where(b => b.AuthorId == 1008).Count()})"); */
+            // создание объекта сущности Группа,
+            // не связанного с БД
             var g1 = new Group() {Name = "911"};
+            // в слое логики добавляем выше созданный объект
             context.Groups.Add(g1);
+            // там же добавляем объект Студент,
+            // ссылающийся на новую группу
             context.Students.Add(new Student() {Name = "The First Student", Group = g1});
+            // все накопившиеся изменения отображаем в слой данных
+            // (сохраняем изменения: две вставки строк в две разные таблицы)
             context.SaveChanges();
+            // получаем весь список групп с количеством студентов в каждой из них
+            // (одна группа с одним студентом)
             context.Groups.Select(g => $"{g.Name} (${g.Students.Count} students)")
                 .ToList()
                 .ForEach(Console.WriteLine);
+            // меняем название группы в объекте, то есть в слое логики
             g1.Name = "911-2";
+            // отображение изменений в слой данных:
+            // формирование и выполнение команды обновления для таблицы Группы,
+            // для единственной группы g1
             context.SaveChanges();
+            // просматриваем изменения, снова прочитав данные из базы
             context.Groups.Select(g => $"{g.Name} (${g.Students.Count} students)")
                 .ToList()
                 .ForEach(Console.WriteLine);
+            // в слое логики удаляем объект студента
             context.Students.Remove(
                 context.Students.Where(s => s.GroupId == g1.Id).SingleOrDefault()
             );
+            // отображаем удаление в БД
             context.SaveChanges();
+            // просматриваем изменения, снова прочитав данные из базы
             context.Groups.Select(g => $"{g.Name} (${g.Students.Count} students)")
                 .ToList()
                 .ForEach(Console.WriteLine);
